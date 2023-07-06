35fb396db15d243301289d04990f1b486f4d9945
leandroKachu <lesilvajunior@hotmail.com>
2023-07-05 23:26:53 -0700

    adjust repository to make a right query

--- src/repository/exchange.go
+++ src/repository/exchange.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"database/sql"
-	"fmt"
 )
 
 type Conversion struct {
@@ -14,49 +13,46 @@ func NewRepositoryConversion(db *sql.DB) *Conversion {
 }
 
 type ConversionResult struct {
-	ValorConvertido float64 `json:"valorConvertido"`
-	SimboloMoeda    string  `json:"simboloMoeda"`
+	ConvertedValue float64 `json:"valorConvertido"`
+	CurrencySymbol string  `json:"simboloMoeda"`
 }
 
-func (repository Conversion) CreateConversion(amount, ExchangeRate, ConvertedValue float64, FromCurrency, CurrencySymbol, ToCurrency string) (interface{}, error) {
+func (repository Conversion) CreateConversion(ExchangeRate, ConvertedValue float64, FromCurrency, CurrencySymbol, ToCurrency string) (interface{}, error) {
 
-	row := repository.db.QueryRow("SELECT DISTINCT id, converted_value, currency_symbol FROM conversions WHERE amount = ? AND from_currency = ? AND to_currency = ? AND exchange_rate = ?", amount, FromCurrency, ToCurrency, ExchangeRate)
+	row := repository.db.QueryRow("SELECT DISTINCT id, converted_value, currency_symbol FROM conversions WHERE from_currency = ? AND to_currency = ? AND exchange_rate = ?", FromCurrency, ToCurrency, ExchangeRate)
 
 	var convertedValue float64
 	var currencySymbol string
 	var id int64
 
 	err := row.Scan(&id, &convertedValue, &currencySymbol)
-	fmt.Println(err)
-
 	if err == nil {
 		result := ConversionResult{
-			ValorConvertido: convertedValue,
-			SimboloMoeda:    currencySymbol,
+			ConvertedValue: convertedValue,
+			CurrencySymbol: currencySymbol,
 		}
 
 		return result, nil
 	} else if err != sql.ErrNoRows {
-		// Ocorreu um erro ao executar a consulta
 		return 0, err
 	}
 
 	statement, erro := repository.db.Prepare(
-		"INSERT INTO conversions (amount, from_currency, to_currency, exchange_rate, converted_value, currency_symbol) VALUES (?, ?, ?, ?, ?, ?)",
+		"INSERT INTO conversions (from_currency, to_currency, exchange_rate, converted_value, currency_symbol) VALUES ( ?, ?, ?, ?, ?)",
 	)
 
 	if erro != nil {
 		return 0, erro
 	}
 	defer statement.Close()
-	_, erro = statement.Exec(amount, FromCurrency, ToCurrency, ExchangeRate, ConvertedValue, CurrencySymbol)
+	_, erro = statement.Exec(FromCurrency, ToCurrency, ExchangeRate, ConvertedValue, CurrencySymbol)
 	if erro != nil {
 		return 0, erro
 	}
 
 	result := ConversionResult{
-		ValorConvertido: ConvertedValue,
-		SimboloMoeda:    CurrencySymbol,
+		ConvertedValue: ConvertedValue,
+		CurrencySymbol: CurrencySymbol,
 	}
 
 	return result, nil
